c7dee9602d1744e4eb20bab9bf34aa5c04bf4676
bot-vini-ofc-2 <78944640+bot-vini-ofc-2@users.noreply.github.com>
2021-02-12 16:43:39 -0400

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,193 @@
+const help = (prefix) => {
+	return `●❯───BOT VINI COMANDOS───❮●
+	
+*╔═══❖•ೋ° °ೋ•❖═══╗*
+
+┃➤Criador : Vinicius 
+┃➤Numero : https://wa.me/5565992559365
+┃➤Grupo do bot : https://chat.whatsapp.com/JXILSMSi7PBE91dXnf5QvS
+
+*╚═══❖•ೋ° °ೋ•❖═══╝*
+	
+◎ ─━──━─❖─━──━─ ◎
+＊✿❀ PRINCIPAIS COMANDOS ❀✿＊
+◎ ─━──━─❖─━──━─ ◎
+
+┃➤Comando: '${prefix}sticker' ou '${prefix}stiker'
+┃➤Função: converter imagem / gif / vídeo em adesivo
+┃➤Como Usar: responder imagem / gif / vídeo ou enviar imagem / gif / vídeo com legenda 
+
+┃➤Comando: '${prefix}sticker nobg' ou '${prefix}stiker nobg'
+┃➤Função: converter imagem em adesivo removendo o fundo
+┃➤Como Usar: responder a imagem, ou enviar com legenda
+
+┃➤Comando: '${prefix}toimg'
+┃➤Função: converter adesivos em imagem
+┃➤Como Usar: responder a sticker 
+
+┃➤Comando:'${prefix}criador'
+┃➤Função: Mostra as informações do criador do bot
+
+◎ ─━──━─❖─━──━─ ◎
+＊✿❀ COMANDOS MEME ❀✿＊
+◎ ─━──━─❖─━──━─ ◎
+
+┃➤Comando: '${prefix}meme'
+┃➤Função: imagens aleatórias de meme [english]
+
+┃➤Comando: '${prefix}memeindo'
+┃➤Função: imagens aleatórias de meme [indo]
+
+◎ ─━──━─❖─━──━─ ◎
+＊✿❀ COMANDOS +18 ❀✿＊
+◎ ─━──━─❖─━──━─ ◎
+
+┃➤ATENÇÃO: Os servidores estão instaveis então caso não envie, mande o comando novamente
+
+┃➤Comando: '${prefix}nekoanime'
+┃➤Função: imagens aleatórias de neko
+
+┃➤Comando: '${prefix}randomkiss'
+┃➤Função: imagens aleatórias de beijo
+
+┃➤Comando: '${prefix}randomhug'
+┃➤Função: imagens aleatórias de abraço
+
+┃➤Comando: '${prefix}randomshota'
+┃➤Função: imagens aleatórias de shota
+
+┃➤Comando '${prefix}randomanime'
+┃➤Função: imagem aleatoria de anime
+
+┃➤Comando '${prefix}randomhentai'
+┃➤Função: imagem aleatoria de hentai
+
+┃➤Comando: '${prefix}nsfwblowjob'
+┃➤Função: gifs de blowjobs de hentai
+
+┃➤Comando: '${prefix}nsfwneko'
+┃➤Função: imagem de hentai neko
+
+┃➤Comando: '${prefix}nsfwtrap'
+┃➤Função: imagem de hentai trap
+
+┃➤Comando: '${prefix}nsfw'
+┃➤Função: Ativa o modo hentai no grupo
+┃➤Como Usar: '${prefix}nsfw 1' para ativar o modo nsfw e '${prefix}nsfw 0' para desativar o modo nsfw
+┃➤Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+┃➤Comando: '${prefix}pack'
+┃➤Função: Enviar um pacote de links +18
+┃➤Como Usar: Basta enviar o comando
+
+┃➤Obs: o comando .loli e .nsfwloli .shota foram removidos devido a o servidor nao estar funcionando
+
+◎ ─━──━─❖─━──━─ ◎
+＊✿❀ COMANDOS DE AUDIO  ❀✿＊
+◎ ─━──━─❖─━──━─ ◎
+
+┃➤Comando: '${prefix}playlist'
+┃➤Função: Mostra a playlist de música
+
+┃➤Comando: '${prefix}gtts'
+┃➤Função: converter texto em fala / áudio
+┃➤Como Usar: '${prefix}gtts [la] [texto]'
+┃➤Exemplo: '${prefix}gtts ja Oni-chan'
+
+◎ ─━──━─❖─━──━─ ◎
+＊✿❀ COMANDOS DE TEXTO ❀✿＊
+◎ ─━──━─❖─━──━─ ◎
+
+┃➤ATENÇÃO: Os servidores estão instaveis então caso não envie, mande o comando novamente
+
+┃➤Comando: '${prefix}textcomands'
+┃➤Função: lista os comandos de texto
+
+┃➤Comando: '${prefix}repeat'
+┃➤Função: Repete o que falar
+┃➤Como Usar: '${prefix}repeat [texto]'
+
+◎ ─━──━─❖─━──━─ ◎
+＊✿❀ COMANDOS MEME ❀✿＊
+◎ ─━──━─❖─━──━─ ◎
+
+┃➤Comando: '${prefix}gimage'
+┃➤Função: Pesquisa no google imagens
+┃➤Como Usar: '${prefix}gimage [texto]'
+
+┃➤Comando: '${prefix}termux'
+┃➤Função: Mostra uma lista de comandos básicos do termux
+
+┃➤Comando: '${prefix}signome'
+┃➤Função: Mostra o significado do seu nome
+┃➤Como usar:'${prefix}signome [nome]'
+┃➤Exemplo:'${prefix}signome ian'
+
+┃➤Comando: '${prefix}nethunter'
+┃➤Função: Mostra um tutorial de como ter um emulador de kali linux no seu termux
+
+┃➤Comando: '${prefix}db'
+┃➤Função: envia links de várias database de travazap
+
+┃➤Comando:'${prefix}wppim'
+┃➤Função: envia links de todos whatsapp imunes
+
+┃➤Comando: '${prefix}url2img'
+┃➤Função: tirar uma captura de tela na web
+┃➤Como Usar: *${prefix}url2img [tip] [url]*
+
+┃➤Comando: '${prefix}simi'
+┃➤Função: sua mensagem será respondida por simi
+┃➤Como Usar: '${prefix}simi [texto]'
+
+┃➤Comando: '${prefix}ocr'
+┃➤Função: pegue o texto na foto
+┃➤Como Usar: responder imagem ou enviar imagem com legenda 
+
+┃➤Comando: '${prefix}wait'
+┃➤Função: Pesquisa o anime na imagem [ O que anime é isso / aquilo ]
+┃➤Como Usar: responder imagem ou enviar imagem com legenda 
+
+┃➤Comando: '${prefix}setprefix'
+┃➤Função: substituir prefixo
+┃➤Como Usar: '${prefix}setprefix [text|optional]' 
+┃➤Exemplo: '${prefix}setprefix ?'
+
+◎ ─━──━─❖─━──━─ ◎
+＊✿❀ COMANDOS  DE GRUPO ❀✿＊
+◎ ─━──━─❖─━──━─ ◎
+
+┃➤Comandos: '${prefix}welcome'
+┃➤Função: Ativa o modo boas-vindas do grupo
+┃➤Como Usar: '${prefix}welcome 1' para ativar o modo welcome e '${prefix}welcome 0' para desativar o modo welcome
+┃➤Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+┃➤Comandos: '${prefix}linkgroup'
+┃➤Função: pegue o link do grupo
+┃➤Como Usar: basta enviar o comando
+┃➤Obs: só pode ser usado quando o bot se torna admin, e quem enviar o comando é admin! 
+
+┃➤Comandos: '${prefix}tagall'
+┃➤Função: marca todos os membros do grupo, incluindo administradores também
+┃➤Como Usar: basta enviar o comando
+┃➤Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+┃➤Comandos: '${prefix}simih'
+┃➤Função: ative o modo simi no grupo
+┃➤Como Usar: '${prefix}simih 1' para ativar o modo simih e '${prefix}simih 0' para desativar o modo simih
+┃➤Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+╲╭━<━━╮╲╲
+╲┃╭╮╭╮┃╲╲.        OBG POR USAR O
+┗┫┏━━┓┣┛╲.              BOT VINI
+╲┃╰━━╯┃ ╲
+╲╰┳━━┳╯╲╲
+╲╲┛╲╲┗╲╲╲
+
+
+`
+}
+
+exports.help = help
+
+
